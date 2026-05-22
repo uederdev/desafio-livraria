b93dbb48fe0ad55db11eca2d28f12c7bdb924ab5
Ueder Carlos Costa Caetano <uedercosta@gmail.com>
2026-05-21 23:38:53 -0300

    Adicionando as classes: Autor, Livro, Biblioteca e Emprestimo;
    Criado ainda Menu principal e DAOGenerico;

--- src/Main.java
+++ src/Main.java
@@ -10,11 +10,9 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         MenuPrincipal.exibir();
-        //while (opcao != 0) {
         System.out.print("Informe a opção desejada: ");
         opcao = sc.nextInt();
         MenuPrincipal.opcaoSelecionada(opcao);
-        //}
     }
 
 }
